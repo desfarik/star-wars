2909d3a45ac423647557e2ccd8920e5f3c4b3522
Dima Astreyko <d.astreyko@itransition.com>
2023-05-08 00:34:11 +0300

    rework id

--- scripts/transform.js
+++ scripts/transform.js
@@ -1,40 +1,20 @@
 import fs from 'fs';
 
-const itemsWithImage = JSON.parse(fs.readFileSync(`./another.data.json`, { encoding: 'UTF-8' }));
-fs.readdirSync(`../api/people`)
+fs.readdirSync(`../api/vehicle`)
     .forEach(fileName => {
-        const data = fs.readFileSync(`../api/people/${fileName}`, { encoding: 'UTF-8' })
+        const data = fs.readFileSync(`../api/vehicle/${fileName}`, { encoding: 'UTF-8' })
+        const [index] = fileName.split('.');
+
         const items = JSON.parse(data);
-        if (Array.isArray(items))
-        {
+        if(Array.isArray(items)) {
             items.forEach((item, index) => {
-                const externalItem = itemsWithImage.find(i=>i.name.toLowerCase() === item.name.toLowerCase());
-                delete item.species;
-                if(externalItem) {
-                    item.wiki = externalItem.wiki;
-                    item.image = externalItem.image;
-                    item.species = externalItem.species;
-                    item.homeworld = externalItem.homeworld;
-                    item.born = externalItem.born;
-                    item.bornLocation = externalItem.bornLocation;
-                    item.died = externalItem.died;
-                    item.diedLocation = externalItem.diedLocation;
-                }
+                delete item.url;
+                item.id = index + 1;
             })
         } else {
-            const externalItem = itemsWithImage.find(i=>i.name.toLowerCase() === items.name.toLowerCase());
-            delete items.species;
-            if(externalItem) {
-                items.wiki = externalItem.wiki;
-                items.image = externalItem.image;
-                items.species = externalItem.species;
-                items.homeworld = externalItem.homeworld;
-                items.born = externalItem.born;
-                items.bornLocation = externalItem.bornLocation;
-                items.died = externalItem.died;
-                items.diedLocation = externalItem.diedLocation;
-            }
+            delete items.url;
+            items.id = +index;
         }
 
-        fs.writeFileSync(`../api/people/${fileName}`, JSON.stringify(items));
+        fs.writeFileSync(`../api/vehicle/${fileName}`, JSON.stringify(items));
     })
